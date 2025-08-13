b77313602bf303800690e416ce113f81f9c6a00a
PilkeySEK <PilkeySEK@gmx.de>
2025-08-13 22:16:45 +0200

    knight movement validation

--- src/move_validation/validator.rs
+++ src/move_validation/validator.rs
@@ -1,5 +1,5 @@
 use crate::{
-    board::{BoardExt, BoardIndex, BoardIndexExt, Color, PieceType},
+    board::{BoardExt, BoardIndexExt, Color, PieceType},
     move_validation::movement::Movement,
 };
 
@@ -8,6 +8,10 @@ pub fn validate_move(m: Movement) -> bool {
         false
     } else {
         let destination_piece = m.movement_info.board.piece_at(m.destination);
+        let start_xy = m.start.to_xy();
+        let destination_xy = m.destination.to_xy();
+        let x_distance = (start_xy.0 as i32 - destination_xy.0 as i32).abs();
+        let y_distance = (start_xy.1 as i32 - destination_xy.1 as i32).abs();
         match m.movement_info.piece_type {
             PieceType::Pawn => {
                 let modifier: i32 = if m.movement_info.piece_color == Color::White {
@@ -15,8 +19,6 @@ pub fn validate_move(m: Movement) -> bool {
                 } else {
                     1
                 };
-                let start_xy = (m.start as BoardIndex).to_xy();
-                let destination_xy = (m.destination as BoardIndex).to_xy();
                 if start_xy.0 != destination_xy.0 {
                     if !m.movement_info.capturing {
                         false
@@ -48,10 +50,6 @@ pub fn validate_move(m: Movement) -> bool {
                 }
             }
             PieceType::King => {
-                let start_xy = m.start.to_xy();
-                let destination_xy = m.destination.to_xy();
-                let x_distance = (start_xy.0 as i32 - destination_xy.0 as i32).abs();
-                let y_distance = (start_xy.1 as i32 - destination_xy.1 as i32).abs();
                 if x_distance > 1 || y_distance > 1 || (x_distance == 0 && y_distance == 0) {
                     false
                 } else if destination_piece.is_none() {
@@ -60,6 +58,19 @@ pub fn validate_move(m: Movement) -> bool {
                     false
                 } else {
                     true
+                }
+            }
+            PieceType::Knight => {
+                if x_distance == 1 && y_distance == 2 || x_distance == 2 && y_distance == 1 {
+                    if destination_piece.is_none() {
+                        true
+                    } else if destination_piece.unwrap().color == m.movement_info.piece_color {
+                        false
+                    } else {
+                        true
+                    }
+                } else {
+                    false
                 }
             }
             _ => todo!(),
