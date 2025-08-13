6b543f2df760234dc92ccd23c094687e4109de50
PilkeySEK <PilkeySEK@gmx.de>
2025-08-13 10:42:29 +0200

    change app to config

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 use eframe::egui::Pos2;
 
-use crate::ChessApp;
+use crate::{config::GameConfig, ChessApp};
 
 pub fn screen_pos_to_board_index(app: &ChessApp, screen_pos: Pos2) -> u16 {
     let new_pos: (u16, u16) = (
@@ -10,11 +10,11 @@ pub fn screen_pos_to_board_index(app: &ChessApp, screen_pos: Pos2) -> u16 {
     new_pos.1 * app.config.board_squares + new_pos.0
 }
 
-pub fn board_index_to_board_xy(app: &ChessApp, index: u16) -> (u16, u16) {
-    let squares = app.config.board_squares;
+pub fn board_index_to_board_xy(config: &GameConfig, index: u16) -> (u16, u16) {
+    let squares = config.board_squares;
     (index % squares, index / squares)
 }
 
-pub fn board_xy_to_board_index(app: &ChessApp, xy: (u16, u16)) -> u16 {
-    app.config.board_squares * xy.1 + xy.0
+pub fn board_xy_to_board_index(config: &GameConfig, xy: (u16, u16)) -> u16 {
+    config.board_squares * xy.1 + xy.0
 }
